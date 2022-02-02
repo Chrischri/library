2c3906bb1759396358b103cf5e4e8218bcc5800f
Chrischri <christian@e-business-conseils.com>
2022-02-02 18:35:11 +0100

    version_originale

---
+++ library_management_system/doc/RELEASE_NOTES.md
@@ -0,0 +1,12 @@
+## Module <library_management_system>
+
+#### 01.02.2021
+#### Version 14.0.1.0.0
+##### ADD
+- Initial commit for Library Management System
+
+#### 03.06.2021
+#### Version 14.0.1.0.1
+##### FIX
+- Bug Fixed
+
---
+++ library_management_system/static/src/js/action_manager.js
@@ -0,0 +1,52 @@
+odoo.define('library_management_system.action_manager', function (require) {
+"use strict";
+
+/**
+ * The purpose of this file is to add the actions of type
+ * 'ir_actions_xlsx_download' to the ActionManager.
+ */
+
+var ActionManager = require('web.ActionManager');
+var framework = require('web.framework');
+var session = require('web.session');
+
+ActionManager.include({
+
+    /**
+     * Executes actions of type 'ir.actions.library.xlsx_download'.
+     *
+     * @private
+     * @param {Object} action the description of the action to execute
+     * @returns {Deferred} resolved when the report has been downloaded ;
+     *   rejected if an error occurred during the report generation
+     */
+    _executeCashXlsxReportDownloadAction: function (action) {
+        framework.blockUI();
+        var def = $.Deferred();
+        session.get_file({
+            url: '/library_xlsx_reports',
+            data: action,
+            success: def.resolve.bind(def),
+            error: (error) => this.call('crash_manager', 'rpc_error', error),
+            complete: framework.unblockUI,
+        });
+        return def;
+    },
+    /**
+     * Overrides to handle the 'ir.actions.library.xlsx_download' actions.
+     *
+     * @override
+     * @private
+     */
+    _handleAction: function (action, options) {
+
+        if (action.type === 'ir.actions.library.xlsx_download') {
+            var state = this._super.apply(this, arguments);
+            action.output_format = 'xlsx'
+            return this._executeCashXlsxReportDownloadAction(action, options);
+        }
+        return this._super.apply(this, arguments);
+    },
+});
+
+});
